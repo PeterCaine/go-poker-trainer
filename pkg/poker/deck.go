2040c456e32cf88d1f4a68d91dcec66d07e862a6
PeterCaine <petercaine@gmail.com>
2025-03-06 09:14:56 +0100

    restructure - separate game from deck

--- pkg/poker/deck.go
+++ pkg/poker/deck.go
@@ -66,38 +66,3 @@ func (d *Deck) Deal(n int)[]Card{
 
 }
 
-type Game struct {
-    Deck Deck
-    PlayerHand []Card
-    CommunityCards []Card
-    CurrentPhase string
-}
-
-func NewGame() *Game {
-    g := &Game {
-        Deck: CreateDeck(),
-        PlayerHand: []Card{},
-        CommunityCards: []Card{},
-        CurrentPhase: "preflop",
-    }
-    g.Deck.ShuffleDeck()
-    g.PlayerHand = g.Deck.Deal(2)
-    return g
-
-}
-
-func (g *Game) DealNextPhase(){
-    switch g.CurrentPhase {
-    case "preflop":
-        g.CommunityCards = g.Deck.Deal(3)
-        g.CurrentPhase = "flop"
-    case "flop":
-        g.CommunityCards = append(g.CommunityCards, g.Deck.Deal(1)[0])
-        g.CurrentPhase = "turn"
-    case "turn":
-        g.CommunityCards = append(g.CommunityCards, g.Deck.Deal(1)[0])
-        g.CurrentPhase = "river"
-    case "river":
-        g.CurrentPhase = "showdown"
-    }
-}
--- pkg/poker/poker.go
+++ pkg/poker/poker.go
@@ -1,1 +1,37 @@
 package poker
+
+type Game struct {
+    Deck Deck
+    PlayerHand []Card
+    CommunityCards []Card
+    CurrentPhase string
+}
+
+func NewGame() *Game {
+    g := &Game {
+        Deck: CreateDeck(),
+        PlayerHand: []Card{},
+        CommunityCards: []Card{},
+        CurrentPhase: "preflop",
+    }
+    g.Deck.ShuffleDeck()
+    g.PlayerHand = g.Deck.Deal(2)
+    return g
+
+}
+
+func (g *Game) DealNextPhase(){
+    switch g.CurrentPhase {
+    case "preflop":
+        g.CommunityCards = g.Deck.Deal(3)
+        g.CurrentPhase = "flop"
+    case "flop":
+        g.CommunityCards = append(g.CommunityCards, g.Deck.Deal(1)[0])
+        g.CurrentPhase = "turn"
+    case "turn":
+        g.CommunityCards = append(g.CommunityCards, g.Deck.Deal(1)[0])
+        g.CurrentPhase = "river"
+    case "river":
+        g.CurrentPhase = "showdown"
+    }
+}
